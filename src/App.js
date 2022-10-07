a5b2f9dc101a389c4fa6d9f20ce84e6b442988f7
NeagaStefan <neagastfean@gmail.com>
2022-10-07 09:08:25 +0300

    v0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Switch>
+            <Route path={'/'} component={Home}/>
+            <Route path={'/login'} component={Login}/>
+            <Route path={'/sign-up'} component={SignUp}/>
+          </Switch>
+        </BrowserRouter>
     </div>
   );
 }
