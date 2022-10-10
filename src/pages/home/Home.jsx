e37486fb803c4dbdb06e7308593486301a8a59f8
NeagaStefan <neagastfean@gmail.com>
2022-10-10 09:52:52 +0300

    v0.2

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React  from "react";
+import {useAuthContext} from "../../hooks/useAuthContext"
 import styles from './Home.module.css'
+import TransactionForm from './TransactionForm'
 
 export default function Home() {
-    return(
-        <div>
-            Home
+    const { user } = useAuthContext();
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}> Transaction list</div>
+
+            <div className={styles.sidebar}>
+                <TransactionForm uid={user.uid}/>
+            </div>
         </div>
     )
 }
